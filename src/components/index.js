6dd390834350d611e00ea13520f2cfdd847fff86
shiixoalei <791919140@qq.com>
2021-02-04 09:44:37 +0800

    框架上传

---
+++ src/components/index.js
@@ -0,0 +1,42 @@
+/*
+ * 描述: 封装组件库
+ * 作者: Jack Chen
+ * 日期: 2020-04-18
+ */
+
+import bgAnimation from './bgAnimation' // 登录界面背景图动画
+import sinan from './sinan' // 司南排名图
+import seamless from './seamless' // 新闻无缝滚动
+import pyramid from './pyramid' // 金字塔动画
+
+
+const components = {
+  bgAnimation,
+  sinan,
+  seamless,
+  pyramid,
+};
+
+const install = (Vue = {}) => {
+  if (install.installed) return;
+  Object.keys(components).forEach(component => {
+    Vue.component(components[component].name, components[component]);
+  });
+
+  install.installed = true;
+};
+
+install.installed = false;
+
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+  install.installed = true;
+}
+
+const Vcomp = {
+  ...components,
+  install
+};
+
+
+export default Vcomp
